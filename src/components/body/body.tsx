d1d3d2ed38893fad3caad71dfa964a88924dc7b4
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-11-14 21:54:53 +0530

    first commit

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -1,46 +1,46 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { deleteData } from '../../redux/reducers/deleteSlice'
-import { getData } from '../../redux/reducers/fetchSlice'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteData } from '../../redux/reducers/deleteSlice';
+import { getData } from '../../redux/reducers/fetchSlice';
+import './body.css';
 
 const Body = () => {
-  const dispatch = useDispatch()
-  const userData = useSelector((state: any) => state.fetch.data.data)
-  console.log('userData', userData && Object.keys(userData))
-  const deleted = useSelector((state: any) => state.delete.data)
+  const dispatch = useDispatch();
+  const userData = useSelector((state: any) => state.fetch.data.data);
+  console.log('userData', userData && Object.keys(userData));
+  const deleted = useSelector((state: any) => state.delete.data);
 
-  console.log('del', deleted && deleted.status)
+  console.log('del', deleted && deleted.status);
 
   useEffect(() => {
-    dispatch(getData())
-  }, [deleted])
+    dispatch(getData());
+  }, [deleted]);
 
   return (
-    <div>
+    <div className="bodyContainer">
       {userData &&
         Object.keys(userData).map((key, index) => {
           return (
-            <div key={index}>
+            <div key={index} className="bodyItems">
               <div className="itemcontainer">
-                <div className="item">{userData[key].name}</div>&nbsp;
-                <div className="item">{userData[key].phone}</div>&nbsp;
+                <div className="item">{userData[key].name}</div>
+                <div className="item">{userData[key].phone}</div>
                 <div className="item">{userData[key].address}</div>
               </div>
               <div className="delete">
                 <button
                   onClick={() => {
-                    dispatch(deleteData(key))
+                    dispatch(deleteData(key));
                   }}
                 >
                   Delete
                 </button>
               </div>
-              <hr />
             </div>
-          )
+          );
         })}
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
