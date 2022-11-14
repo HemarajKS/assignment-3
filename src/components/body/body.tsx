f1c4631c96804626537f7d21f0102af1931d392d
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-11-14 17:34:13 +0530

    last anu

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -3,10 +3,29 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getData } from '../../redux/reducers/fetchSlice'
 
 const Body = () => {
-  const dispatch = useDispatch()
-
   const userData = useSelector((state: any) => state.fetch.data.data)
-  return <div>{JSON.stringify(userData)}</div>
+  console.log('userData', userData && Object.keys(userData))
+
+  return (
+    <div>
+      {userData &&
+        Object.keys(userData).map((key, index) => {
+          return (
+            <div key={index}>
+              <div className="itemcontainer">
+                <div className="item">{userData[key].name}</div>&nbsp;
+                <div className="item">{userData[key].phone}</div>&nbsp;
+                <div className="item">{userData[key].address}</div>
+              </div>
+              <div className="delete">
+                <button>Delete</button>
+              </div>
+              <hr />
+            </div>
+          )
+        })}
+    </div>
+  )
 }
 
 export default Body
